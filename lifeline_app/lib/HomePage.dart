70dc37e347116e7ee03b3ef753d2dc29cce5fe76
OYGWeek5 <yong0002@student.monash.edu>
2020-10-26 03:09:32 +0800

    First Aider nearby

--- lifeline_app/lib/HomePage.dart
+++ lifeline_app/lib/HomePage.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lifeline_app/MapPage.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -13,16 +15,28 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'This is Home Page',
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
+      floatingActionButton : ListView(children: <Widget>[
+        Container(
+          //height: 150,
+          margin: EdgeInsets.symmetric(vertical: 130.0),
+          padding:EdgeInsets.symmetric(horizontal:30),
+          child:RaisedButton(
+            child: Text('Emergency',style: TextStyle(color: Colors.white)),
+            color: Colors.red,
+            splashColor: Colors.lightBlue,
+            shape: CircleBorder(),
+            padding: EdgeInsets.all(100.0),
+            onPressed: (){
+              Navigator.push(context,
+                MaterialPageRoute(builder:(context)=>MapPage()),
+              );
+            },
+          ),
+        ),
+      ],)  ,
     );
   }
 }
--- lifeline_app/lib/main.dart
+++ lifeline_app/lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'PharmacyPage.dart';
 import 'DieticianPage.dart';
 import 'HomePage.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
