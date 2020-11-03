9fc9e6a88bc540631b8da0880e7fa9297a16c006
atan0064 <atan0064@student.monash.edu>
2020-11-04 00:25:34 +0700

    improvised product detail with tab bar

--- lifeline_app/lib/DrugDetail.dart
+++ lifeline_app/lib/DrugDetail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class DrugDetail extends StatelessWidget {
   dynamic currentDrug;
@@ -9,36 +10,164 @@ class DrugDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(title: Text(currentDrug['name'])),
       body: SingleChildScrollView(
         child: Container(
-          child: Column(
-            children: <Widget>[new Image.asset("assets/images/${currentDrug["image"]}",height: 300),
-                              Text("\n"),
-                              Text(currentDrug['name'],style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),),
-                              Text("RM"+currentDrug['price'].toString()+"/item", style: const TextStyle(fontSize: 17, color: Colors.grey),),
-                              Text("\n"),
-                              Align(
-                                alignment: Alignment(-0.875, 0),
-                                child: Text("Description:",style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+          width: width,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: height * 0.55,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/${currentDrug["image"]}"),
+                        fit: BoxFit.cover)),
+                child: Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                    Colors.black.withOpacity(0.9),
+                    Colors.black.withOpacity(0.5),
+                    Colors.black.withOpacity(0.0),
+                    Colors.black.withOpacity(0.0),
+                    Colors.black.withOpacity(0.5),
+                    Colors.black.withOpacity(0.9),
+                  ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+                ),
+              ),
+              Container(
+                width: width,
+                margin: EdgeInsets.only(top: height * 0.5),
+                padding: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  color: currentDrug['color'],
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      currentDrug['name'],
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    DefaultTabController(
+                      length: 3,
+                      child: Column(
+                        children: <Widget>[
+                          TabBar(
+                            labelColor: Colors.white,
+                            labelStyle: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                            unselectedLabelColor: Colors.grey[400],
+                            unselectedLabelStyle: TextStyle(
+                                fontWeight: FontWeight.normal, fontSize: 15),
+                            indicatorSize: TabBarIndicatorSize.label,
+                            indicatorColor: Colors.transparent,
+                            indicator: UnderlineTabIndicator(),
+                            tabs: <Widget>[
+                              Tab(
+                                child: Text("Description"),
                               ),
-                              Container(
-                                padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
-                                child: Text(currentDrug['description'],style: const TextStyle(fontSize: 17, color: Colors.grey),textAlign: TextAlign.justify,),
+                              Tab(
+                                child: Text("Precaution"),
                               ),
-                              Align(
-                                alignment: Alignment(-0.875, 0),
-                                child: Text("Precautions:",style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                              Tab(
+                                child: Text("Pharmacy"),
                               ),
-                              Container(
-                                padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
-                                child: Text(currentDrug['precautions'],style: const TextStyle(fontSize: 17, color: Colors.grey),textAlign: TextAlign.justify,),
-                              ),                              
-                              ],
-            )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                        ],
+                      )
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      currentDrug['description'],
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white60,
+                          fontWeight: FontWeight.normal,
+                          letterSpacing: 0.5,
+                          wordSpacing: 1.5),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Price",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            Text(
+                              "RM"+currentDrug['price'].toString(),
+                              style: TextStyle(
+                                  color: Colors.white60,
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        RaisedButton(
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          color: Colors.orange[800],
+                          padding: EdgeInsets.fromLTRB(35, 15, 35, 15),
+                          child: Text(
+                            "Add to cart",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                
+              ),
+              Positioned(
+                left: 30,
+                top: height * 0.05,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.keyboard_backspace,
+                    size: 42,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-        ),
+      ),
     );
   }
 }
+
--- lifeline_app/lib/ListOfDrugs.dart
+++ lifeline_app/lib/ListOfDrugs.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 const DRUGS_DATA = [
   {
     "name": "Panadol Blue",
@@ -6,6 +9,7 @@ const DRUGS_DATA = [
     "image": "panadolblue.png",
     "description": "Panadol Advance 500 mg Tablets are a mild analgesic and antipyretic, and are recommended for the treatment of most painful and febrile conditions, for example, headache including migraine and tension headaches, toothache, backache, rheumatic and muscle pains, dysmenorrhoea, sore throat, and for relieving the fever, aches and pains of colds and flu.",
     "precautions": "Contains paracetamol. Do not use with any other paracetamol-containing products.",
+    "color": Colors.indigo
   },
     {
     "name": "Nexium",
